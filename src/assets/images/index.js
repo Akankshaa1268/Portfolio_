a26ce516188971b575fd48e6c2c5eae43cd8af84
Akankshaa1268 <akankshapal1207@gmail.com>
2025-05-31 17:38:40 +0530

    Initial commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,27 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import logo from './logo.svg'
+import iitd from './iitd.png'
+import igdtuw from '/igdtuw.png'
+import python from './iitd.png'
+import C from './iitd.png'
+import figma from './iitd.png'
+
+
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo,
+    iitd,
+    igdtuw,
+    python,
+    C,
+    figma
+}
---
+++ src/constants/index.js
@@ -0,0 +1,179 @@
+import { meta, shopify, starbucks, tesla, C, figma, python, iitd, igdtuw } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: C,
+        name: "C++",
+        type: "Programming Language",
+    },
+    
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: python,
+        name: "Python",
+        type: "Backend",
+    },
+    {
+        imageUrl: figma,
+        name: "Figma",
+        type: "Frontend",
+    },
+  
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+  
+   
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+   
+];
+
+export const experiences = [
+    {
+        title: " Summer Intern",
+        company_name: " Indira Gandhi Delhi Technical University for Women",
+        icon: igdtuw,
+        iconBg: "#accbe1",
+        date: "June 2025 - Present",
+        points: [
+        "Part of an 8-week internship organized by the Department of AI & DS, covering coursework in HTML/CSS, JavaScript, React.js with TypeScript, Node.js, Express.js, and MongoDB",
+        "Undergoing training in MERN stack development with AI integrations using HuggingFace and OpenAI APIs, focused onbuilding scalable, intelligent web applications.",
+           
+        ],
+    },
+    {
+        title: " STEM Mentorship Mentee",
+        company_name: " Indian Institute of Technology, Delhi",
+        icon: iitd,
+        iconBg: "#fbc3bc",
+        date: "Feb 2023 - Nov 2023",
+        points: [
+            "Selected as 1 of 32 mentees for a year-long STEM program; explored emerging technologies like cryptography, network security, and drone tech via hands-on workshops and lectures.",
+            "Engaged with IIT Delhi faculty and research scholars through lab visits and interactive sessions to gain exposure to real-world STEM research and applications.",
+           
+        ],
+    },
+  
+  
+];
+
+export const socialLinks = [
+    {
+        name: 'Resume',
+        iconUrl: arrow,
+        link: 'https://drive.google.com/file/d/1mToeaOuuNGv2OLqyRERRe_FKMhprFuft/view?usp=sharing',
+
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/akankshapal1268',
+    },
+     {
+        name: 'Leetcode',
+        iconURL: leetcode, 
+        link: 'https://leetcode.com/akanksha1268/',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Akankshaa1268',
+    },
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    
+    
+   
+
+];
+
+export const projects = [
+    {
+        iconURL: mirror,
+        theme: 'btn-back-red',
+        name: ' Mirror - Web app',
+        description: ' AI-powered web app to scan files (PDF, CSV, DOCX, etc.) for threats, anomalies, and risks; delivers explainable results and interactive visual reports for non-technical users.',
+        link: 'https://github.com/singh-bhawana/Mirror-',
+    },
+    {
+        iconUrl: tuo,
+        theme: 'btn-back-green',
+        name: 'Tuo - Educational App',
+        description: ' Mobile learning app that embeds YouTube content in a distraction-free interface with sequenced lessons, quizzes, progress tracking, and personalized content recommendations.',
+        link: 'https://drive.google.com/file/d/1Z8leUwX58SJxYmOfXli34Hk4lN46ngk_/view?usp=sharing',
+    },
+    {
+        iconUrl: cardigan_logo,
+        theme: 'btn-back-blue',
+        name: ' Cardigan ',
+        description: ' UI/UX prototype for a social platform connecting users with NGOs through secure blockchain-backed volunteering, donation tracking, credit-score incentives, and community-driven features.',
+        link: 'https://github.com/Akankshaa1268/Cardigan.git',
+    },
+    
+  
+];
+ 
